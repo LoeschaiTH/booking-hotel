970ed533ff318250980b7df2fe03f5ff29b4eebc
LoeschaiTH <loeschai.s@knacx.com>
2025-05-11 00:03:32 +0700

    initialize React app with Vite, add routing, and create Home and About pages

---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "../pages/home/index";
+import About from "../pages/about/index";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+}
+
+export default App;
